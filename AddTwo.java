dc1f0f25b0526a319d0d60a56fbd6bf77f335c4e
noam <noam.kyram@post.runi.ac.il>
2023-12-21 18:58:23 +0200

    made AddTwo

--- AddTwo.java
+++ AddTwo.java
@@ -2,7 +2,14 @@
  * Adds two given integers and prints the result in a fancy way.
  */
 public class AddTwo {
-	public static void main(String[] args) {
-		// Put your code here	
+	public static void main(String [] args){
+		// get params a and b using args, and casting to integers
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+
+		int sum = a+b; // set to a new variable the v value of a + b
+
+		System.out.println(a+ " + " +b + " = "+ sum); // print the result as requested
+
 	}
 }
