09494b4c7f60980e00d89e717171eb51fd8ef724
noam <noam.kyram@post.runi.ac.il>
2023-12-21 19:03:15 +0200

    made linearEq mission

--- LinearEq.java
+++ LinearEq.java
@@ -1,9 +1,17 @@
-/*
- * Solves linear equations of the form a⋅x + b = c.
- * The program gets a, b, and c as command-line arguments,
- * computes x, and prints the result.
- * Treats the three arguments as well as the computed value as double values
- */
 public class LinearEq {
-	// Put your code here
+
+    public static void main(String[] args) {
+        double a = Double.parseDouble(args[0]); // get param a from args and cast to double
+        double b = Double.parseDouble((args[1])); // get param b from args and cast to double
+        double c = Double.parseDouble((args[2])); // get param c from args and cast to double
+
+        double x = (c-b)/a; // calculate the value of x (using 3 doubles)
+
+        // print the result at the requested format
+        System.out.println(a+" * x + "+b +" = "+c);
+        System.out.println("x = "+x);
+
+    }
+
 }
+
